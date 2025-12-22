cf0dd3b6fdd7e6ac7c0997dfee76cce7d201bf87
Viktor <viktor.avgn@yandex.ru>
2025-12-22 23:32:27 +0700

    реализована фильтрация + get-параметры для фильтрации

---
+++ app/ui/Filter.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { useSearchParams, useRouter, usePathname } from 'next/navigation'
+import { useState, useRef, useEffect } from 'react'
+
+export default function Filter() {
+  const [isOpen, setIsOpen] = useState(false)
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  // ---
+  const dropdownRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        isOpen &&
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [isOpen])
+
+  // ---
+
+  const updateFilter = (value: string) => {
+    const params = new URLSearchParams(searchParams)
+
+    if (value) {
+      params.set('category', value)
+    } else {
+      params.delete('category')
+    }
+
+    router.replace(`${pathname}?${params.toString()}`)
+  }
+
+  return (
+    <div className="catalog-button" ref={dropdownRef}>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        <span className="catalog-button_burger"></span>
+        <span className="catalog-button_text">Каталог</span>
+      </button>
+      <div className="catalog" style={{ display: isOpen ? 'block' : 'none' }}>
+        <ul className="catalog-list">
+          <li onClick={() => updateFilter('Игровая приставка')}>
+            Игровая приставка
+          </li>
+          <li onClick={() => updateFilter('Периферия для ПК')}>
+            Периферия для ПК
+          </li>
+          <li onClick={() => updateFilter('Игры и софт')}>Игры и софт</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+
